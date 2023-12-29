2f0efc8a556f7048f2281a70088d47379259e352
Ahmed <ahmedabdallabeltagy@gmail.com>
2023-12-30 00:46:26 +0200

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,23 +2,50 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { SUCCESS, FAIL, ERROR } from "../utils/httpStatusText.js";
 import appError from "../utils/appError.js";
+import jwt from "jsonwebtoken";
 
-export const signUp = async(req,res,next)=>{
-    const {username,email,password} = req.body;
+export const signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
     const oldEmail = await User.findOne({ email: email });
     const oldUserName = await User.findOne({ username: username });
-    if (oldEmail || oldUserName ) {
-        const error = appError.create("User Already Exists", 404, FAIL)
+    if (oldEmail) {
+        const error = appError.create("Email Already Used", 404, FAIL)
         return next(error);
     }
-    const hashedPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({username, email, password:hashedPassword});
-    // if user exits in db return msg 
-    // if email exits in db return msg 
-    try{
+    if (oldUserName) {
+        const error = appError.create("Username Already Used", 404, FAIL)
+        return next(error);
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
         await newUser.save();
-        res.status(201).json({msg:"User created successfully"})
-    }catch(err){
+        res.status(201).json({ status: SUCCESS})
+    }
+    catch (err) {
         next(err)
+    }
+}
+
+export const signIn = async(req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){       
+        const error = appError.create("User Not Found!",404, ERROR)
+        return next(error);
+        }
+        const matchedPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!matchedPassword){
+            const error = appError.create("Wrong credentials!",401, FAIL)
+            return next(error);
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JTW_SECRET);
+        const {password:pass , ...rest}= validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200)
+        .json(rest)
+    } catch (error) {
+        next(error)
     }
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { signUp } from "../controllers/auth.controller.js";
+import { signUp,signIn } from "../controllers/auth.controller.js";
 
 router.post("/signUp",signUp)
+router.post("/signIn",signIn)
+
 
 export default router;
